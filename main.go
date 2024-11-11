9e3dafe1277ed45a5d370988934d859f1cb69b0b
hectorProgrammer10 <aguilar.uri1504@gmail.com>
2024-11-10 18:52:28 -0600

    primera version

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
+// main.go
 package main
 
 import (
 	"estacionamiento/src/scenes"
+	"math/rand"
 	"os"
+	"sync"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 )
 
+const (
+	capacidadEstacionamiento = 20
+	numVehiculos             = 100
+)
+
+var (
+	mutexPuerta        sync.Mutex
+	lugaresDisponibles = capacidadEstacionamiento
+	estacionamiento    = make([]bool, capacidadEstacionamiento) // indica si el lugar está ocupado
+)
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Estacionamiento GOOOO")
 
@@ -17,12 +33,21 @@ func main() {
 	myWindow.Resize(fyne.NewSize(900, 400))
 	myWindow.SetFixedSize(true)
 
-	//--esena
+	// Crear escena y cargar imagen de fondo
 	scene := scenes.NewScene(myWindow)
 	scene.Init()
+	cargarImagenFondo(scene, "src/views/estacionamiento.png")
 
-	//estacionamiento
-	imagePath := "src/views/estacionamiento.png"
+	// Simulación de llegada de vehículos
+	for i := 0; i < numVehiculos; i++ {
+		go crearVehiculo(i, scene)                                          // Lanza cada vehículo como una goroutine
+		time.Sleep(time.Duration(rand.ExpFloat64() * float64(time.Second))) // Simula llegada en distribución Poisson
+	}
+
+	myWindow.ShowAndRun()
+}
+
+func cargarImagenFondo(scene *scenes.Scene, imagePath string) {
 	imgFile, err := os.Open(imagePath)
 	if err != nil {
 		panic("No se pudo cargar la imagen: " + err.Error())
@@ -31,11 +56,54 @@ func main() {
 	img := canvas.NewImageFromFile(imagePath)
 	img.FillMode = canvas.ImageFillOriginal
 	img.Resize(fyne.NewSize(600, 250))
-
 	img.Move(fyne.NewPos(300, 150))
-
-	//------------
-
 	scene.AddImage(img)
-	myWindow.ShowAndRun()
+}
+
+// Función que simula la creación de un vehículo
+func crearVehiculo(id int, scene *scenes.Scene) {
+	for {
+		mutexPuerta.Lock()
+		if lugaresDisponibles > 0 {
+			// Hay espacio disponible, intenta entrar
+			lugaresDisponibles--
+			lugar := ocuparLugar()
+			mutexPuerta.Unlock() // Libera el control de la puerta para que otros vehículos entren o salgan
+
+			if lugar != -1 {
+				scene.MostrarVehiculo(id, lugar)                        // Muestra el vehículo en la UI
+				time.Sleep(time.Duration(rand.Intn(3)+3) * time.Second) // Tiempo de estacionamiento aleatorio entre 3 y 5 segundos
+
+				// Salida del vehículo
+				mutexPuerta.Lock()
+				lugaresDisponibles++
+				liberarLugar(lugar)
+				scene.QuitarVehiculo(id) // Remueve el vehículo de la UI
+				mutexPuerta.Unlock()
+			}
+			return
+		} else {
+			// Estacionamiento lleno, espera a que un lugar se libere
+			mutexPuerta.Unlock()
+			time.Sleep(500 * time.Millisecond) // Reintenta después de un breve descanso
+		}
+	}
+}
+
+// Busca un lugar vacío y lo marca como ocupado, devuelve el índice del lugar o -1 si no hay lugar
+func ocuparLugar() int {
+	for i := 0; i < len(estacionamiento); i++ {
+		if !estacionamiento[i] {
+			estacionamiento[i] = true
+			return i
+		}
+	}
+	return -1
+}
+
+// Libera el lugar en el estacionamiento
+func liberarLugar(lugar int) {
+	if lugar >= 0 && lugar < len(estacionamiento) {
+		estacionamiento[lugar] = false
+	}
 }
--- src/scenes/esena.go
+++ src/scenes/esena.go
@@ -1,7 +1,9 @@
 package scenes
 
 import (
+	"fmt"
 	"image/color"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -11,24 +13,21 @@ import (
 type Scene struct {
 	scene     fyne.Window
 	container *fyne.Container
+	vehiculos map[int]*canvas.Image
 }
 
 func NewScene(scene fyne.Window) *Scene {
-	return &Scene{scene: scene, container: nil}
+	return &Scene{scene: scene, container: nil, vehiculos: make(map[int]*canvas.Image)}
 }
 
 func (s *Scene) Init() {
-
-	// Crear un rectángulo de fondo con color personalizado
+	// Crear fondo blanco
 	rect := canvas.NewRectangle(color.White)
 	rect.Resize(fyne.NewSize(900, 400))
 	rect.Move(fyne.NewPos(0, 0))
-
-	// Colocar el rectángulo de fondo y los widgets dentro de un contenedor
 	s.container = container.NewWithoutLayout(rect)
 	s.scene.SetContent(s.container)
 }
-
 func (s *Scene) AddWidget(widget fyne.Widget) {
 	s.container.Add(widget)
 	s.container.Refresh()
@@ -37,4 +36,40 @@ func (s *Scene) AddWidget(widget fyne.Widget) {
 func (s *Scene) AddImage(image *canvas.Image) {
 	s.container.Add(image)
 	s.container.Refresh()
+}
+
+var posiciones = []fyne.Position{
+	// Lugares 1-10 (en la fila superior)
+	{X: 100, Y: 250}, {X: 150, Y: 250}, {X: 200, Y: 250}, {X: 250, Y: 250},
+	{X: 300, Y: 250}, {X: 350, Y: 250}, {X: 400, Y: 250}, {X: 450, Y: 250},
+	{X: 500, Y: 250}, {X: 550, Y: 250},
+	// Lugares 11-20 (en la segunda fila)
+	{X: 100, Y: 300}, {X: 150, Y: 300}, {X: 200, Y: 300}, {X: 250, Y: 300},
+	{X: 300, Y: 300}, {X: 350, Y: 300}, {X: 400, Y: 300}, {X: 450, Y: 300},
+	{X: 500, Y: 300},
+}
+
+func (s *Scene) MostrarVehiculo(id, lugar int) {
+	imgPath := "assets/autoConcurrente.png"
+	imgFile, err := os.Open(imgPath)
+	if err != nil {
+		fmt.Println("Error cargando la imagen del vehículo:", err)
+		return
+	}
+	defer imgFile.Close()
+
+	vehiculo := canvas.NewImageFromFile(imgPath)
+	vehiculo.Resize(fyne.NewSize(48, 48))
+	vehiculo.Move(posiciones[lugar])
+	s.container.Add(vehiculo)
+	s.vehiculos[id] = vehiculo
+	s.container.Refresh()
+}
+
+func (s *Scene) QuitarVehiculo(id int) {
+	if vehiculo, exists := s.vehiculos[id]; exists {
+		s.container.Remove(vehiculo)
+		delete(s.vehiculos, id)
+		s.container.Refresh()
+	}
 }
